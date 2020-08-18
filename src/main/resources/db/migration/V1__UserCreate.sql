b47d456dbbf640d4885eed0bd090eb8677b0dbae
arsonal_xu <31698025+arsodroid@users.noreply.github.com>
2020-08-18 23:23:26 +0800

    git init

---
+++ src/main/resources/db/migration/V1__UserCreate.sql
@@ -0,0 +1,9 @@
+-- 电话请用字符串，id用long会报错请用bigint
+create table USER(
+    ID bigint primary key auto_increment,
+    NAME varchar(100) not null ,
+    TEL varchar(20) unique ,
+    AVATAR_URL varchar(1024),
+    CREATED_AT timestamp,
+    UPDATED_AT timestamp
+);
